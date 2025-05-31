89ed74d08c676c408e9149daf257bb8390a4d1c0
Tomokatsu Iguchi <github@igtm.link>
2025-05-31 12:59:52 +0900

    fix

--- android/app/src/main/java/com/podcastapp/rainbowview/RainbowView.kt
+++ android/app/src/main/java/com/podcastapp/rainbowview/RainbowView.kt
@@ -1,76 +1,58 @@
 package com.podcastapp.rainbowview
 
 import android.content.Context
+import android.graphics.Canvas
 import android.graphics.Color
+import android.graphics.Paint
 import android.os.Handler
-import android.os.Looper
-import android.util.AttributeSet
 import android.view.View
 import com.facebook.react.bridge.Arguments
-import com.facebook.react.bridge.ReactContext
-import com.facebook.react.uimanager.events.RCTEventEmitter
+import com.facebook.react.uimanager.ThemedReactContext
 
-class RainbowView @JvmOverloads constructor(
-    context: Context,
-    attrs: AttributeSet? = null,
-    defStyleAttr: Int = 0
-) : View(context, attrs, defStyleAttr) {
-
-    private val colors = arrayOf(
-        Color.RED, Color.parseColor("#FF7F00"), Color.YELLOW,
-        Color.GREEN, Color.BLUE, Color.parseColor("#4B0082"), Color.parseColor("#9400D3")
-    )
-    
-    private var currentColorIndex = 0
-    private var handler: Handler? = null
-    private var colorChangeRunnable: Runnable? = null
-    
+class RainbowView(context: ThemedReactContext) : View(context) {
     var updateMillis: Int = 1000
-        set(value) {
-            field = value
-            if (isRunning) {
-                stopChangeColor()
-                startChangeColor()
-            }
-        }
-    
+    private var currentColorIndex = 0
+    private val colors = listOf(
+        Color.RED, Color.YELLOW, Color.GREEN, Color.CYAN, Color.BLUE, Color.MAGENTA
+    )
+    private val paint = Paint()
+    private val handler = Handler()
     private var isRunning = false
 
-    init {
-        setBackgroundColor(colors[0])
+    private val colorChangeRunnable = object : Runnable {
+        override fun run() {
+            currentColorIndex = (currentColorIndex + 1) % colors.size
+            invalidate()
+            sendColorChangedEvent()
+            if (isRunning) {
+                handler.postDelayed(this, updateMillis.toLong())
+            }
+        }
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        paint.color = colors[currentColorIndex]
+        canvas.drawRect(0f, 0f, width.toFloat(), height.toFloat(), paint)
     }
 
     fun startChangeColor() {
-        if (isRunning) return
-        isRunning = true
-        
-        handler = Handler(Looper.getMainLooper())
-        colorChangeRunnable = object : Runnable {
-            override fun run() {
-                currentColorIndex = (currentColorIndex + 1) % colors.size
-                val newColor = colors[currentColorIndex]
-                setBackgroundColor(newColor)
-                sendColorChangedEventToJs(newColor)
-                handler?.postDelayed(this, updateMillis.toLong())
-            }
+        if (!isRunning) {
+            isRunning = true
+            handler.post(colorChangeRunnable)
         }
-        handler?.postDelayed(colorChangeRunnable!!, updateMillis.toLong())
     }
 
     fun stopChangeColor() {
         isRunning = false
-        handler?.removeCallbacks(colorChangeRunnable!!)
-        handler = null
-        colorChangeRunnable = null
+        handler.removeCallbacks(colorChangeRunnable)
     }
 
-    private fun sendColorChangedEventToJs(color: Int) {
-        val arguments = Arguments.createMap().apply {
-            putString("color", "0x${color.toUInt().toString(16)}")
-        }
-        (context as ReactContext)
-            .getJSModule(RCTEventEmitter::class.java)
-            .receiveEvent(id, "onColorChanged", arguments)
+    private fun sendColorChangedEvent() {
+        val event = Arguments.createMap()
+        event.putString("color", String.format("#%06X", 0xFFFFFF and colors[currentColorIndex]))
+        (context as? ThemedReactContext)?.getJSModule(com.facebook.react.uimanager.events.RCTEventEmitter::class.java)
+            ?.receiveEvent(id, "onColorChanged", event)
     }
 
     override fun onDetachedFromWindow() {
